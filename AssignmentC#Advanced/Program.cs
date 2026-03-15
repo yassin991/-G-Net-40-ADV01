ea3a1ed004937d16fb08dff2e307cf11bb16c186
yassin991 <yassenelsherbiny69@gmail.com>
2026-03-15 15:26:03 +0200

    Question20

--- AssignmentC#Advanced/Program.cs
+++ AssignmentC#Advanced/Program.cs
@@ -278,7 +278,52 @@ namespace AssignmentC_Advanced
         //{
         //}
         #endregion
+        #region Question20
+        //public class Cache<TKey, TValue>
+        //{
+        //    private class CacheItem
+        //    {
+        //        public TValue Value;
+        //        public DateTime ExpirationTime;
+        //    }
 
+        //    private Dictionary<TKey, CacheItem> cache = new Dictionary<TKey, CacheItem>();
+
+        //    public void Add(TKey key, TValue value, TimeSpan duration)
+        //    {
+        //        cache[key] = new CacheItem
+        //        {
+        //            Value = value,
+        //            ExpirationTime = DateTime.Now.Add(duration)
+        //        };
+        //    }
+
+        //    public TValue Get(TKey key)
+        //    {
+        //        if (cache.ContainsKey(key))
+        //        {
+        //            var item = cache[key];
+
+        //            if (DateTime.Now <= item.ExpirationTime)
+        //                return item.Value;
+
+        //            cache.Remove(key);
+        //        }
+
+        //        return default(TValue);
+        //    }
+
+        //    public bool Contains(TKey key)
+        //    {
+        //        return cache.ContainsKey(key);
+        //    }
+
+        //    public void Remove(TKey key)
+        //    {
+        //        cache.Remove(key);
+        //    }
+        //}
+        #endregion
     }
 
 
