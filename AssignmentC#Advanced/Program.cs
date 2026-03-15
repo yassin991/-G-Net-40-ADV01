c2f9daeefc2ebf9315954256d85c0d5bd0a1d974
yassin991 <yassenelsherbiny69@gmail.com>
2026-03-15 15:18:54 +0200

    Question05

--- AssignmentC#Advanced/Program.cs
+++ AssignmentC#Advanced/Program.cs
@@ -75,6 +75,24 @@ namespace AssignmentC_Advanced
         //}
 
         #endregion
+
+        #region Question05
+        //Q5: Write a generic method FindMax<T> that finds maximum value
+        //    public static T FindMax<T>(T a, T b) where T : IComparable<T>
+        //{
+        //    if (a.CompareTo(b) > 0)
+        //        return a;
+        //    else
+        //        return b;
+
+        //}
+        //int max = FindMax(10, 20);
+        //Console.WriteLine(max);
+
+        #endregion
+
+
+
     }
 
 
